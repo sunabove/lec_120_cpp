b22341530321f3eb04337f2cfda13e622b5e3731
sunabove <sunabove@nate.com>
2018-04-21 09:57:09 +0900

    **

---
+++ lec_sol/lec_07/fibonacci.cpp
@@ -0,0 +1,46 @@
+// lec_07.cpp : Defines the entry point for the console application.
+#include "stdafx.h"
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+typedef double number;
+
+const number epsilon = 0.000000001;
+const number e = epsilon;
+
+number f(number x) {
+	return x * x - 4;
+}
+
+number derivative(number x) {
+	number der = (f(x + e) - f(x)) / e;
+	return der;
+}
+
+int mainNewtonRaphson( int argc, char ** argv ) {
+	FILE * out = stdout;
+	const char * LINE = "\n**********************************************************";
+	fprintf(out, LINE);
+	fprintf(out, "\n     Newton-Raphson Method Program");
+	fprintf(out, LINE);
+
+	number x = 6.0;
+	number y = 0;
+	int index = 0;
+
+	do {
+		y = f(x);
+		fprintf(out, "\n[%03d] f(%.20f) = %.20f", index, x, y);
+		x = x - (y / derivative(x));
+		index++;
+	} while (y > epsilon || -y > epsilon);
+
+	fprintf(out, LINE);
+	fflush(out);
+	fprintf(out, "\nEnter any key to continue. ");
+	_getch();
+
+	return 0;
+}
+// end of file
