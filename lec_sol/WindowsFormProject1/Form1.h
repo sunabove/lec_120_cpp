9bf856a7c6f33bf1b45f4bdc976af1039b27a9ab
sunabove <sunabove@nate.com>
2018-06-05 09:53:40 +0900

    **

--- lec_sol/WindowsFormProject1/Form1.h
+++ lec_sol/WindowsFormProject1/Form1.h
@@ -34,6 +34,10 @@ namespace CppCLR_WinformsProjekt {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::ComboBox^  comboBox1;
+	protected:
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::ComboBox^  comboBox2;
 
 	private:
 		/// <summary>
@@ -48,14 +52,48 @@ namespace CppCLR_WinformsProjekt {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->comboBox2 = (gcnew System::Windows::Forms::ComboBox());
 			this->SuspendLayout();
-
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Location = System::Drawing::Point(56, 83);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(121, 21);
+			this->comboBox1->TabIndex = 0;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(228, 148);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// comboBox2
+			// 
+			this->comboBox2->FormattingEnabled = true;
+			this->comboBox2->Location = System::Drawing::Point(56, 148);
+			this->comboBox2->Name = L"comboBox2";
+			this->comboBox2->Size = System::Drawing::Size(121, 21);
+			this->comboBox2->TabIndex = 2;
+			// 
+			// Form1
+			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(284, 261);
+			this->ClientSize = System::Drawing::Size(487, 365);
+			this->Controls->Add(this->comboBox2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->comboBox1);
 			this->Name = L"Form1";
 			this->Text = L"Form1";
 			this->ResumeLayout(false);
+
 		}
 #pragma endregion
 	};
